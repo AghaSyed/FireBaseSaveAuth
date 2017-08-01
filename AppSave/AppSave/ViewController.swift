3407f09f54288edd57013d2214d216e6ea484846
Syed Hasnain <syedhasnain.thedevclan@gmail.com>
2017-08-02 00:24:32 +0500

    login

--- AppSave/AppSave/ViewController.swift
+++ AppSave/AppSave/ViewController.swift
@@ -23,9 +23,62 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func saveButtonClicked(_ sender: UIButton) {
+        if emailTextField.text == "" || passwordTextField.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            if segmentButton.selectedSegmentIndex == 1 {
+                Auth.auth().signIn(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
+                    
+                    if error == nil {
+                        
+                        //Print into the console if successfully logged in
+                        print("You have successfully logged in")
+                        
+                        //Go to the HomeViewController if the login is sucessful
+                      //  let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                       // self.present(vc!, animated: true, completion: nil)
+                        
+                    } else {
+                        
+                        //Tells the user that there is an error and then gets firebase to tell them the error
+                        let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                        
+                        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(defaultAction)
+                        
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                }
+            } else {
+                Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+                    
+                    if error == nil {
+                        print("You have successfully signed up")
+                        //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
+                        
+                        // let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                        //   self.present(vc!, animated: true, completion: nil)
+                        
+                    } else {
+                        let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                        
+                        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(defaultAction)
+                        
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                }
+            }
+            
     }
   
 
-
+    }
 }
 

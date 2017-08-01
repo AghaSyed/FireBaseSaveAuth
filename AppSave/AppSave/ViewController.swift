4fd1b952fd854d99628bf511ddbdef00f6192f80
Syed Hasnain <syedhasnain.thedevclan@gmail.com>
2017-08-01 23:02:21 +0500

    App Save

---
+++ AppSave/AppSave/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  AppSave
+//
+//  Created by Syed Qamar Abbas on 01/08/2017.
+//  Copyright © 2017 Syed Qamar Abbas. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+class ViewController: UIViewController {
+
+    @IBOutlet var segmentButton: UISegmentedControl!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func saveButtonClicked(_ sender: UIButton) {
+    }
+  
+
+
+}
+

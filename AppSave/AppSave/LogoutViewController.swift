f154b2bf7e83e92d4caa6a373e85bdedb6f3c718
Syed Hasnain <syedhasnain_bsse0035@live.com>
2017-08-02 13:07:28 +0500

    DataBase Implemented

--- AppSave/AppSave/LogoutViewController.swift
+++ AppSave/AppSave/LogoutViewController.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 import FirebaseAuth
-class LogoutViewController: UIViewController {
+import FirebaseDatabase
+class LogoutViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var myTableView: UITableView!
+    @IBOutlet weak var myTextField: UITextField!
     @IBOutlet weak var userEmailLabel: UILabel!
+    
+    var ref: DatabaseReference?
+    var handle: DatabaseHandle?
+    var myList: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         self.userEmailLabel.text = "Welcome " + (Auth.auth().currentUser?.email)!
+        
+        ref = Database.database().reference()
+        handle = ref?.child("list").observe(.childAdded, with: { (snapshot) in
+            if let item = snapshot.value as? String {
+                self.myList.append(item)
+                self.myTableView.reloadData()
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,19 +36,23 @@ class LogoutViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func didTapSaveButton(_ sender: Any) {
+        if myTextField.text != "" {
+            ref?.child("list").childByAutoId().setValue(myTextField.text)
+            myTextField.text = ""
+        }
+    }
 
     @IBAction func didTapLogoutButton(_ sender: Any) {
         try! Auth.auth().signOut()
         self.performSegue(withIdentifier: "sugueLogout", sender: self)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myList.count
     }
-    */
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = myList[indexPath.row]
+        return cell
+    }
 }
